665e6375a9ea827a00cddbfb21af3ed5b89205a3
Russell Long <russelllongj@gmail.com>
2022-05-06 19:49:35 +1000

    change the way grid draws

--- src/board.py
+++ src/board.py
@@ -16,25 +16,22 @@ class Board:
 
     def draw_static(self, surface):
         # Main game area
-        draw_grid(surface, self.settings["board_x_pos"], self.settings["board_y_pos"], self.settings["num_cols"], self.settings["num_rows"], self.settings["board_width"], self.settings["board_height"],  self.settings["grid_thickness"] )
+        draw_grid(surface, self.settings["board_x_pos"], self.settings["board_y_pos"], self.settings["num_cols"], self.settings["num_rows"], self.settings["cell_size"],  self.settings["grid_thickness"] )
 
         # preview grid
         preview_x = self.settings["board_x_pos"] + self.settings["board_width"] + self.settings["preview_gap_from_main_grid"]
-        preview_y = self.settings["board_y_pos"] - self.settings["grid_thickness"]
-        draw_grid(surface, preview_x, preview_y, self.preview_col_count, self.preview_row_count * self.settings["preview_count"], 100, 100 * self.settings["preview_count"], self.settings["grid_thickness"])
+        draw_grid(surface, preview_x, self.settings["board_y_pos"], self.preview_col_count, self.preview_row_count * self.settings["preview_count"], self.settings["cell_size"], self.settings["grid_thickness"])
 
 
-def draw_grid(surface, posx: float, posy:float, num_cols: int, num_rows: int, grid_width: float, grid_height: float, line_width: int):
-    cell_vertical_separation = (grid_height - line_width * num_rows) / num_rows
-    cell_horizontal_separation = (grid_width - line_width * num_cols) / num_cols
-
+def draw_grid(surface, posx: float, posy:float, num_cols: int, num_rows: int, cell_size: float, line_width: int):
+    cell_separation = cell_size + line_width
     for i in range(num_cols + 1):
-        start_x = posx + i * (cell_horizontal_separation + line_width)
+        start_x = posx + i * cell_separation
         start_pos = (start_x, posy)
-        end_pos = (start_x, posy + grid_height)
+        end_pos = (start_x, posy + cell_separation * num_rows)
         pg.draw.line(surface, colours["grid"], start_pos, end_pos, line_width)
     for j in range(num_rows + 1):
-        start_y = posy + j * (cell_vertical_separation + line_width)
+        start_y = posy + j * cell_separation
         start_pos = (posx, start_y)
-        end_pos = (posx + grid_width, start_y)
+        end_pos = (posx + num_cols * cell_separation, start_y)
         pg.draw.line(surface, colours["grid"], start_pos, end_pos, line_width)
