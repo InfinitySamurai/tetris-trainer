8d555c9b160971376fc150b3643acd3c7dbb4400
Russell Long <russelllongj@gmail.com>
2022-05-07 19:35:50 +1000

    Fix up some data issues for tetronimoes

--- src/data/tetronimoData.py
+++ src/data/tetronimoData.py
@@ -12,20 +12,20 @@ class Tetronimoes(Enum):
 
 tetronimo_colours = {
     Tetronimoes.I: (0, 153, 230),
-    Tetronimoes.J: (200, 100, 0),
-    Tetronimoes.L: (0, 0, 150),
-    Tetronimoes.Z: (200, 0, 0),
-    Tetronimoes.S: (0, 180, 0),
+    Tetronimoes.J: (0, 0, 150),
+    Tetronimoes.L: (200, 100, 0),
+    Tetronimoes.S: (200, 0, 0),
+    Tetronimoes.Z: (0, 180, 0),
     Tetronimoes.O: (200, 200, 0),
     Tetronimoes.T: (150, 0, 150),
 }
 
 tetronimo_shapes = {
-    Tetronimoes.I: np.array([[0, 0, 0, 0], [0, 0, 0, 0], [Tetronimoes.I.value, Tetronimoes.I.value, Tetronimoes.I.value, Tetronimoes.I.value], [0, 0, 0, 0]]),
-    Tetronimoes.J: np.array([[0, 0, 0], [Tetronimoes.J.value, Tetronimoes.J.value, Tetronimoes.J.value], [0, 0, Tetronimoes.J.value]]),
-    Tetronimoes.L: np.array([[0, 0, 0], [Tetronimoes.L.value, Tetronimoes.L.value, Tetronimoes.L.value], [Tetronimoes.L.value, 0, 0]]),
-    Tetronimoes.Z: np.array([[0, 0, 0], [0, Tetronimoes.Z.value, Tetronimoes.Z.value], [Tetronimoes.Z.value, Tetronimoes.Z.value, 0]]),
-    Tetronimoes.S: np.array([[0, 0, 0], [Tetronimoes.S.value, Tetronimoes.S.value, 0], [0, Tetronimoes.S.value, Tetronimoes.S.value]]),
+    Tetronimoes.I: np.array([[0, 0, 0, 0], [Tetronimoes.I.value, Tetronimoes.I.value, Tetronimoes.I.value, Tetronimoes.I.value], [0, 0, 0, 0], [0, 0, 0, 0]]),
+    Tetronimoes.J: np.array([[Tetronimoes.J.value, 0, 0], [Tetronimoes.J.value, Tetronimoes.J.value, Tetronimoes.J.value], [0, 0, 0]]),
+    Tetronimoes.L: np.array([[0, 0, Tetronimoes.L.value], [Tetronimoes.L.value, Tetronimoes.L.value, Tetronimoes.L.value], [0, 0, 0]]),
+    Tetronimoes.S: np.array([[0, Tetronimoes.Z.value, Tetronimoes.Z.value], [Tetronimoes.Z.value, Tetronimoes.Z.value, 0], [0, 0, 0]]),
+    Tetronimoes.Z: np.array([[Tetronimoes.S.value, Tetronimoes.S.value, 0], [0, Tetronimoes.S.value, Tetronimoes.S.value], [0, 0, 0]]),
     Tetronimoes.O: np.array([[Tetronimoes.O.value, Tetronimoes.O.value], [Tetronimoes.O.value, Tetronimoes.O.value]]),
-    Tetronimoes.T: np.array([[0, 0, 0], [Tetronimoes.T.value, Tetronimoes.T.value, Tetronimoes.T.value], [0, Tetronimoes.T.value, 0]]),
+    Tetronimoes.T: np.array([[0, Tetronimoes.T.value, 0], [Tetronimoes.T.value, Tetronimoes.T.value, Tetronimoes.T.value], [0, 0, 0]]),
 }
