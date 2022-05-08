8bcbb7a935bfaec6e537feb9ef54c9b531fc3efb
Russell Long <russelllongj@gmail.com>
2022-05-08 22:40:52 +1000

    Change s and z around

--- src/data/tetronimoData.py
+++ src/data/tetronimoData.py
@@ -34,8 +34,8 @@ tetronimo_shapes = {
     Tetronimoes.I: np.array([[0, 0, 0, 0], [Tetronimoes.I.value, Tetronimoes.I.value, Tetronimoes.I.value, Tetronimoes.I.value], [0, 0, 0, 0], [0, 0, 0, 0]]),
     Tetronimoes.J: np.array([[Tetronimoes.J.value, 0, 0], [Tetronimoes.J.value, Tetronimoes.J.value, Tetronimoes.J.value], [0, 0, 0]]),
     Tetronimoes.L: np.array([[0, 0, Tetronimoes.L.value], [Tetronimoes.L.value, Tetronimoes.L.value, Tetronimoes.L.value], [0, 0, 0]]),
-    Tetronimoes.S: np.array([[0, Tetronimoes.S.value, Tetronimoes.S.value], [Tetronimoes.S.value, Tetronimoes.S.value, 0], [0, 0, 0]]),
-    Tetronimoes.Z: np.array([[Tetronimoes.Z.value, Tetronimoes.Z.value, 0], [0, Tetronimoes.Z.value, Tetronimoes.Z.value], [0, 0, 0]]),
+    Tetronimoes.Z: np.array([[0, Tetronimoes.Z.value, Tetronimoes.Z.value], [Tetronimoes.Z.value, Tetronimoes.Z.value, 0], [0, 0, 0]]),
+    Tetronimoes.S: np.array([[Tetronimoes.S.value, Tetronimoes.S.value, 0], [0, Tetronimoes.S.value, Tetronimoes.S.value], [0, 0, 0]]),
     Tetronimoes.O: np.array([[Tetronimoes.O.value, Tetronimoes.O.value], [Tetronimoes.O.value, Tetronimoes.O.value]]),
     Tetronimoes.T: np.array([[0, Tetronimoes.T.value, 0], [Tetronimoes.T.value, Tetronimoes.T.value, Tetronimoes.T.value], [0, 0, 0]]),
 }
